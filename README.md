92014ecb8de50f26b677092f0459fc512104feb8
22APRIL <git@stackblitz.com>
2025-12-17 09:51:18 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reading-a-book
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/22APRIL/reading-a-book)
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-2xl text-center font-bold py-6">
+      Welcome to <span className="text-blue-700">React + Tailwind</span>
+    </h1>
+  );
+}
+
+export default App;
